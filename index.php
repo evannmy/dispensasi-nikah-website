41e840dc788499b017bd93aa6f57e8b98d640773
evan <evanmaulanayafi1@gmail.com>
2024-10-15 15:03:12 +0700

    add index page and add logo in image folder

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Permohonan Dispensasi Nikah</title>
+</head>
+<body>
+  <nav>
+    <img src="images/sidoarjo-regency-logo.svg" alt="Logo Kabupaten Sidoarjo">
+    <p>
+      KECAMATAN PORONG
+      <br>
+      KABUPATEN SIDOARJO
+    </p>
+  </nav>
+
+  <h1>Selamat Datang di Website Dispensasi Nikah Kecamatan Porong</h1>
+  <p>
+    Untuk membuat surat dispensasi nikah silahkan lengkapi data yang diperlukan.
+    <br>
+    Klik tombol dibawah ini untuk mengisi data
+  </p>
+
+  <div>
+    <p>Lanjut</p>
+  </div>
+</body>
+</html>
