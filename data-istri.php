57c0ffc77570b62d508e517a6d089847f2a0cfe5
evan <evanmaulanayafi1@gmail.com>
2024-10-17 10:01:35 +0700

    add style for wife's data entry form and husband's data entry form

--- data-istri.php
+++ data-istri.php
@@ -3,45 +3,62 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dispensasi Nikah</title>
+    <title>Dispensasi Nikah - Data Istri</title>
+    <link rel="stylesheet" href="styles/form-data.css">
   </head>
   <body>
-    <p>Masukkan data calon istri</p>
 
-    <form action="" method="post">
-      <!-- input name -->
-      <label for="name">Nama Lengkap: </label>
-      <input type="text" name="name" id="name">
+    <main>
+      <nav>
+        <img class="logo" src="images/sidoarjo-regency-logo.svg" alt="Logo Kabupaten Sidoarjo">
+        <p>
+          KECAMATAN PORONG
+          <br>
+          KABUPATEN SIDOARJO
+        </p>
+      </nav>
+    
+      <p class="header">Permohonan Surat Dispensasi Nikah</p>
+      <p>Masukkan Data Calon Suami</p>
 
-      <!-- input place of birth -->
-      <label for="placeOfBirth">Tempat Lahir: </label>
-      <input type="text" name="placeOfBirth" id="placeOfBirth">
+      <form action="data-istri.php" method="post">
+        <!-- input name -->
+        <label for="name">Nama Lengkap: </label>
+        <input type="text" name="name" id="name" placeholder="Masukkan nama" required>
 
-      <!-- input date of birth-->
-      <label for="dateOfBirth">Tanggal Lahir: </label>
-      <input type="date" name="dateOfBirth" id="dateOfBirth">
+        <!-- input place of birth -->
+        <label for="placeOfBirth">Tempat Lahir: </label>
+        <input type="text" name="placeOfBirth" id="placeOfBirth" placeholder="Masukkan lahir" required>
 
-      <!-- select religion -->
-      <label for="religion">Agama: </label>
-      <select name="religion" id="religion">
-        <option value="islam">Islam</option>
-        <option value="kristen">Kristen</option>
-        <option value="hindu">Hindu</option>
-        <option value="buddha">Buddha</option>
-        <option value="konghucu">Konghucu</option>
-      </select>
+        <!-- input date of birth-->
+        <label for="dateOfBirth">Tanggal Lahir: </label>
+        <input type="date" name="dateOfBirth" id="dateOfBirth" required>
 
-      <!-- input job -->
-      <label for="job">Pekerjaan: </label>
-      <input type="text" name="job" id="job">
+        <!-- select religion -->
+        <label for="religion">Agama: </label>
+        <select name="religion" id="religion">
+          <option value="islam">Islam</option>
+          <option value="kristen">Kristen</option>
+          <option value="hindu">Hindu</option>
+          <option value="buddha">Buddha</option>
+          <option value="konghucu">Konghucu</option>
+        </select>
 
-      <!-- married status -->
-      <label for="marriedStatus">Status Nikah: </label>
-      <input type="text" name="marriedStatus" id="marriedStatus">
+        <!-- input job -->
+        <label for="job">Pekerjaan: </label>
+        <input type="text" name="job" id="job" placeholder="Masukkan pekerjaan calon suami" required>
 
-      <!-- input address -->
-      <label for="address">Alamat: </label>
-      <textarea name="address" id="address"></textarea>
-    </form>
+        <!-- married status -->
+        <label for="marriedStatus">Status Nikah: </label>
+        <input type="text" name="marriedStatus" id="marriedStatus" placeholder="Masukkan status nikah saat ini" required>
+
+        <!-- input address -->
+        <label for="address">Alamat: </label>
+        <textarea name="address" id="address" rows="3" cols="40" required></textarea>
+
+        <input type="submit" name="submit" value="simpan">
+      </form>
+    </main>
+    
   </body>
 </html>
